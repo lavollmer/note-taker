3505aa5acfb3e213baeda8c43e1d578d5cae6433
lavollmer <141365306+lavollmer@users.noreply.github.com>
2023-10-30 18:22:51 -0500

    added HW instructions

--- server.js
+++ server.js
@@ -1,3 +1,20 @@
+/*
+  1. A route that sends the home page
+  2. A route that sends the notes page
+
+  3. Make routes matching the fetch requests coming from the client-side code
+
+  db.json is the make-believe database
+
+  When a request comes in for the data:
+    - Read the file (fs.readFile)
+
+  When a request comes in to make a new note:
+    - Read the file 
+    - Add the new data 
+    - Write a new version of that file
+*/
+
 const express = require('express');
 const path = require('path');
 const fs = require('fs');
@@ -9,3 +26,17 @@ app.use(express.static("public"));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+// Send out the home page
+app.get("/", (req, res) => res.sendFile(path.join(__dirname, "public/index.html")));
+
+app.post("/api/newuser", (req, res) => {
+  fs.writeFile("signup.txt", req.body.newuser, (err) => {
+    if (err) return res.json({ status: "error" })
+    res.json({ status: "success" })
+  })
+})
+
+app.listen(PORT, () =>
+  console.log(`Express server listening on port ${PORT}!`)
+);

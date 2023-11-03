e1af5d84efdbafb7c45e1abf1f5a4f336133c08e
lavollmer <141365306+lavollmer@users.noreply.github.com>
2023-11-02 21:33:40 -0500

    changed it back to data

--- server.js
+++ server.js
@@ -67,7 +67,7 @@ app.post("/api/notes", (req, res) => {
       const newNote = {
         id: varID,
         title: req.body.title,
-        text: req.body.text
+        data: req.body.text
       }
 
       //add new ID number and new note to db.json
